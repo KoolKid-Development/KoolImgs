c1896901faba125be1811ab9a53f98b03135bb3a
KoolKid <93000739+ItzKoolKid96@users.noreply.github.com>
2022-10-18 11:11:14 +0200

    Fixed

---
+++ config-example.py
@@ -0,0 +1,23 @@
+# Welcome to KoolIMGS Cofiguration
+# In this file you can edit your ImageUploader
+
+#Lets start up with our domain fill in your domain
+#You can use an ip or an domain
+domain = 'https://i.kool-kid.xyz'
+#In here you will need to enter the port that you want to 
+#forward with nginx 
+port = '1234'
+#API Key is the key to delete images and upload them!
+api_key = 'API_KEY'
+#Uploader name is your hosting uploader name ex: KoolCloud, KoolCDN, MyImageUploader
+uploader_name = 'KoolImgs'
+#Author name here you can put your name like KoolKidDev 
+author_name = 'KoolKidDev'
+#Your path where you want to store the screenshots
+storage_folder = 'static/screenshots/images'
+#Website url
+website = 'https://kool-kid.xyz'
+#Github url
+github = 'https://kool-kid.xyz/github'
+#Discord url
+discord = 'https://discord.kool-kid.xyz'
